0c12c172e0d17a2d95eaf67d36edcceb86e27ab8
cl-ire <s85609@htw-dresden.de>
2023-12-05 10:30:58 +0100

    fix

--- old/setup.py
+++ old/setup.py
@@ -1,3 +1,7 @@
+import os
+from glob import glob
+from setuptools import setup
+
 from setuptools import find_packages, setup
 
 package_name = 'ros2_for_waveshare_alphabot2'
@@ -7,9 +11,11 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py')))
+        ['libexec/ros2_for_waveshare_alphabot2', glob(os.path.join('libexec', '*'))]
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -18,14 +24,6 @@ setup(
     description='',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    
-    package_data={
-        package_name: [
-            'msg/*.msg',
-            'launch/*.py'
-        ],
-    },
-
     entry_points={
         'console_scripts': [
             'ir_control = ros2_for_waveshare_alphabot2.ir_control:main',
