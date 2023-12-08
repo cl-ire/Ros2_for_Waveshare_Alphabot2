0a6f71ab5287566c3da453b0f607840c3dd2fe15
cl-ire <s85609@htw-dresden.de>
2023-12-08 10:58:00 +0100

    fix

---
+++ ros2_for_waveshare_alphabot2/test3.py
@@ -0,0 +1,80 @@
+import rclpy
+from rclpy.node import Node
+import time
+from sensor_msgs.msg import Image
+from std_msgs.msg import String, Int32MultiArray, Float32, Float32MultiArray
+from geometry_msgs.msg import Twist
+
+
+
+class MovementTest(Node):
+    def __init__(self):
+        super().__init__('movement_test')
+        #create the subscriber 
+        self.subscription = self.create_subscription(
+            String,                     #data type
+            '/joystick',                 #topic published by camera_opencv_node
+            self.listener_callback,     #function to notify that a mesage was recived
+            5)                          #queue size amount of the stored mesages  
+        self.subscription
+
+        self.servo_msg_hold = [0, 0]
+        # Message publishers
+        self.move_pub = self.create_publisher(Twist, 'cmd_vel', 4)
+
+
+
+    def listener_callback(self, msg):
+        # Messages
+        imput = msg.data
+        self.get_logger().info("Imput recived: {}".format(imput)) 
+        
+        # channel 0 = pan 
+		# channel 1 = tilt
+        
+
+        if imput == "Center":
+            self.servo_msg_hold = [0, 0]
+            servo_msg = [0, 0]
+        elif imput == "Up":
+            servo_msg = [0, 5]
+        elif imput == "Right":
+            servo_msg = [5, 0]
+        elif imput == "Left":
+            servo_msg = [-5, 0]
+        elif imput == "Down":
+            servo_msg = [0, -5]
+
+        self.servo_msg_hold[0] = self.servo_msg_hold[0] + servo_msg[0]
+        self.servo_msg_hold[1] = self.servo_msg_hold[1] + servo_msg[1]
+
+        servo_msg_sent = Int32MultiArray()
+        
+        move_msg = Twist()
+        move_msg.linear.x = 0.5
+        move_msg.angular.z = 0.5
+        self.move_pub.publish(move_msg)
+
+        self.get_logger().info("Data sent to Servo: {}".format(self.servo_msg_hold))
+        servo_msg_sent.data = self.servo_msg_hold
+        self.servo_pub.publish(servo_msg_sent)
+                        
+        time.sleep(0.2)
+
+
+
+def main(args=None):
+    
+    rclpy.init()
+    movement_test = MovementTest()
+    rclpy.spin(movement_test)    
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    movement_test.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
